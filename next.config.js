ebd41855c61d47c7d2f6a9ac956bf0586ffada7b
Oleksandr Hubar <oleksandr.hubar@ekreative.com>
2022-11-07 17:32:46 +0200

    add pwa support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,4 +12,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
